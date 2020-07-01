a7be5f7fcf4ee0938fa395a7e6cb0b2db4454848
David Polania <15016075+davidpolaniaac@users.noreply.github.com>
2020-07-01 18:51:23 -0500

    add async test

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -11,4 +11,16 @@ describe('testing async ', () => {
             expect(data.name).toEqual('Rick Sanchez');
         });
     });
+
+    test('failed request to an api', async() => {
+        const apiError = 'https://httpstat.us/404';
+        const request = getDataFromApi(apiError);
+
+        await expect(request).rejects.toEqual(Error('Request failed with status code 404'));
+    });
+
+    test('Solve or reject hello', async() => {
+        await expect(Promise.resolve('hello')).resolves.toBe('hello');
+        await expect(Promise.reject('error')).rejects.toBe('error');
+    });
 });
