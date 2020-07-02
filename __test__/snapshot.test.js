83a50ae703e51f883b3795d0ea9bdf7ae66b7a0c
David Polania <15016075+davidpolaniaac@users.noreply.github.com>
2020-07-01 19:57:38 -0500

    add test for snapchot exceptions

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,17 @@ describe('snapshot', () => {
     test('snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('snapshot exceptions', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+            name: "David"
+        }
+
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number),
+            createAt: expect.any(Date)
+        });
+    });
 });
